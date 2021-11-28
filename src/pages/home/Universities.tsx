b79949023a40e851e1dfdaac854bfc4e4cbe96be
Atanas Chachev <atanaschachev1@gmail.com>
2021-11-28 20:10:50 +0200

    Adding the virtualized list into Animated holder component

--- src/pages/home/Universities.tsx
+++ src/pages/home/Universities.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView, VirtualizedList, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 import { Store } from '@src/models/store/store';
 import { University } from '@src/models/store/universities';
-import { Card } from '@src/components';
+import { AnimatedHolder, Card } from '@src/components';
 import { ThemeColors } from '@src/styles/colors';
 
 type VirtualizedItem = {
@@ -20,27 +20,29 @@ const Universities = () => {
 
   return (
     <SafeAreaView style={styles.holder}>
-      <VirtualizedList
-        data={universities}
-        initialNumToRender={2}
-        renderItem={({ item }: VirtualizedItem) => (
-          <Card
-            numberOfLikes={item.university.likes}
-            title={item.university.name}
-            image={item.university.image}
-            onPress={() => {
-              console.log(item.university);
-            }}
-          />
-        )}
-        keyExtractor={(item: KeyExtractorType, index: number) =>
-          index.toString()
-        }
-        getItemCount={() => universities.length}
-        getItem={(data: University[], index: number) => ({
-          university: data[index],
-        })}
-      />
+      <AnimatedHolder outputRangeFirst={40} delay={400} duration={400}>
+        <VirtualizedList
+          data={universities}
+          initialNumToRender={2}
+          renderItem={({ item }: VirtualizedItem) => (
+            <Card
+              numberOfLikes={item.university.likes}
+              title={item.university.name}
+              image={item.university.image}
+              onPress={() => {
+                console.log(item.university);
+              }}
+            />
+          )}
+          keyExtractor={(item: KeyExtractorType, index: number) =>
+            index.toString()
+          }
+          getItemCount={() => universities.length}
+          getItem={(data: University[], index: number) => ({
+            university: data[index],
+          })}
+        />
+      </AnimatedHolder>
     </SafeAreaView>
   );
 };
